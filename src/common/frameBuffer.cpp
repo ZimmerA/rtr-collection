fb49f09b91e0ea914a55a554102d1ec5ca10349e
Adrian Zimmer <z.adrian1995@gmail.com>
2019-02-01 15:10:31 +0100

    Added frameBuffer class

---
+++ src/common/frameBuffer.cpp
@@ -0,0 +1,24 @@
+#include "frameBuffer.h"
+
+FrameBufferTraits::value_type FrameBufferTraits::create()
+{
+    value_type handle;
+    glGenFramebuffers(1, &handle);
+    return handle;
+}
+
+void FrameBufferTraits::destroy(FrameBufferTraits::value_type handle)
+{
+    glDeleteFramebuffers(1, &handle);
+}
+
+void FrameBuffer::attachTexture2D(GLenum attachment, GLuint texture, const Texture &tex, GLint level)
+{
+    bind();
+    glFramebufferTexture2D(GL_FRAMEBUFFER, attachment, GL_TEXTURE_2D, tex.handle, level);
+}
+
+void FrameBuffer::bind()
+{
+    glBindFramebuffer(GL_FRAMEBUFFER, handle);
+}
---
+++ src/common/frameBuffer.h
@@ -0,0 +1,23 @@
+#ifndef RTR_COLLECTION_FRAMEBUFFER_H
+#define RTR_COLLECTION_FRAMEBUFFER_H
+#include <glad/glad.h>
+#include "openGLObjectBase.h"
+#include "texture.h"
+
+struct FrameBufferTraits {
+    typedef GLuint value_type;
+
+    static value_type create();
+
+    static void destroy(value_type handle);
+};
+
+class FrameBuffer : public OpenGLObjectBase<FrameBufferTraits> {
+public:
+    void bind();
+
+    void attachTexture2D(GLenum attachment, GLuint texture, const Texture &tex, GLint level);
+};
+
+
+#endif //RTR_COLLECTION_FRAMEBUFFER_H
